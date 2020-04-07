67bc3f51a10c770de8b7a2e7d73dbb5a4418a0eb
LXZ_svn_sync <lixizhi@yeah.net>
2020-04-07 12:08:13 +0800

    added haqi code block

--- Mod/HaqiMod/main.lua
+++ Mod/HaqiMod/main.lua
@@ -11,6 +11,7 @@ local HaqiMod = commonlib.gettable("Mod.HaqiMod");
 ]]
 local HaqiMod = commonlib.inherit(commonlib.gettable("Mod.ModBase"),commonlib.gettable("Mod.HaqiMod"));
 
+local block_id = 10518;
 function HaqiMod:ctor()
 end
 
@@ -28,6 +29,30 @@ end
 
 function HaqiMod:init()
 	LOG.std(nil, "info", "HaqiMod", "plugin initialized");
+	-- register a new block item, id < 10512 is internal items, which is not recommended to modify. 
+	GameLogic.GetFilters():add_filter("block_types", function(xmlRoot) 
+		local blocks = commonlib.XPath.selectNode(xmlRoot, "/blocks/");
+		if(blocks) then
+			NPL.load("(gl)Mod/HaqiMod/ItemHaqiCodeBlock.lua");
+			blocks[#blocks+1] = {name="block", attr={ name="HaqiCodeBlock",
+				id = block_id, item_class="ItemNplMicroRobotBlock", text=L"哈奇编辑器",
+				icon = "Texture/blocks/codeblock_on.png",
+			}}
+			LOG.std(nil, "info", "HaqiMod", "HaqiCodeBlock is registered");
+
+		end
+		return xmlRoot;
+	end)
+
+	-- add block to category list to be displayed in builder window (E key)
+	GameLogic.GetFilters():add_filter("block_list", function(xmlRoot) 
+		for node in commonlib.XPath.eachNode(xmlRoot, "/blocklist/category") do
+			if(node.attr.name == "tool" or node.attr.name == "character") then
+				node[#node+1] = {name="block", attr={name="HaqiCodeBlock"} };
+			end
+		end
+		return xmlRoot;
+	end)
 end
 
 function HaqiMod:OnLogin()
